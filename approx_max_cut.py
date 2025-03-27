0a1322fde16b1803bf241f9cdd79402a7279a041
m2schief <schief254@gmail.com>
2025-03-27 16:59:28 -0400

    q2 full

--- approx_max_cut.py
+++ approx_max_cut.py
@@ -1,6 +1,28 @@
 from collections import defaultdict
-import heapq
 # Main Script - Fetch Inputs, do outputs from here. Run directly. 
+
+def greedy_max_cut(adj_list):
+    # Greedily select node who will most increase cut size
+    # Value indicates how much cut will increase by addition
+    # Initialized as node degree
+    node_values = {u: len(adj_list[u]) for u in adj_list}
+
+    S = set()
+
+    while True:
+        # get node with highest value
+        u = max(node_values, key=node_values.get)
+        
+        if node_values[u] <= 0: # adding to set won't increase cut
+            break
+
+        S.add(u)
+        node_values[u] = 0 # adding u no longer increases cut size
+
+        for v in adj_list[u]:
+            node_values[v] -= 1
+        
+    return S
 
 def main():
     
@@ -12,9 +34,11 @@ def main():
     for i in range(m):
         u, v = map(int, input().split())
         edgeAdjList[u].append(v)
+        edgeAdjList[v].append(u)
 
-
-    print("placeholder")
+    greedy_cut = greedy_max_cut(edgeAdjList)
+    print(len(greedy_cut))
+    print(*greedy_cut)
 
 if __name__ == "__main__":
     main()
